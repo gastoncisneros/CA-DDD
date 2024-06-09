206a575a6bfee51a002a2928041d451bc4574e86
GCisneros-Everse <gcisneros@e-verse.co>
2024-06-09 15:03:58 -0300

    Application layer setup with Mediatr and IApplicationRespository

---
+++ Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ClearArchitecture.Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Data
+{
+    // Implemented in Infrastructure
+    public interface IApplicationDbContext
+    {
+        DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Application/DependencyInyection.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application
+{
+    public static class DependencyInyection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
